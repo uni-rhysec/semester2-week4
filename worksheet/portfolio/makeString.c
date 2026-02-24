b626aba67a53fff74fd17c5462b7c0871fb5449d
uni-rhysec <rfrx0712@leeds.ac.uk>
2026-02-24 18:51:23 +0000

    worksheet 2.4

--- worksheet/portfolio/makeString.c
+++ worksheet/portfolio/makeString.c
@@ -1,7 +1,7 @@
 
 /*
-Name:
-Student ID:
+Name: Rhys Carter
+Student ID: 201974757
 */
 
 #include <stdio.h>
@@ -11,7 +11,20 @@ int main( int argc, char **argv ) {
 
     // define appropriate data to hold your answer
 
+    char buffer[50] = {};
+    char dash[10] = {'-'};
+
     // process the command-line data using appropriate string functions
+    // printf("%d\n",argc);
+
+    for (int i=1; i<argc; i++) {
+        strcat(buffer,argv[i]);
+        // printf("pass\n");
+        if (i < argc - 1) {
+            strcat(buffer,dash);
+        }
+        
+    }
 
     printf("%s\n",buffer); // use only this single print statement in your submitted code
 
